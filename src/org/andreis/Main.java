ff09f996f2800981305eab3b1373096178cfb10a
AndreiS <a3356677@gmail.com>
2022-07-06 08:30:27 -0700

    Server for 1 user.

--- src/org/andreis/Main.java
+++ src/org/andreis/Main.java
@@ -5,24 +5,38 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
-
+    static Socket socket = null;
+    static DataInputStream in;
+    static DataOutputStream out;
     public static void main(String[] args) {
-        Socket socket = null;
+
+
         try (ServerSocket serverSocket = new ServerSocket(8189)) {
             System.out.println("Сервер запущен, ожидаем подключения...");
             socket = serverSocket.accept();
             System.out.println("Клиент подключился");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
             while (true) {
                 String str = in.readUTF();
                 System.out.println(str);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+        new Thread(() -> {
+            Scanner scan = new Scanner(System.in);
+            String msg = scan.nextLine();
+            try {
+                out.writeUTF(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 }
